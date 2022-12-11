6765a1c002c017db7014aede45d73d82d005df00
LondaNH <117847738+LondaNH@users.noreply.github.com>
2022-12-10 19:15:18 -0500

    added project screenshot

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
 // Assignment code here
+var characterLength = 8;
+var responsesArr = [];
 
+var specialCharacterArr = ["!", "@", "#", "$", "%", "^", "&", "*", "?", ")", "(", "/", "+", "-", "~"];
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  
+  if(correctPrompts) {
+  var newPassword = generatePassword();
+  passwordText.value = newPassword;
+} else {
+  passwordText.value = "Your password does not meet criteria requirements. Please try again :)";
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
+
+function generatePassword () {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * responsesArr.length);
+    password = password + responsesArr[randomIndex];
+  }
+  return password;
+
+}
+
+function getPrompts(){
+  responsesArr = [];
+
+    characterLength = parseInt(prompt("How many characters would you like? Please choose between 8-128 characters."));
+
+    if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert("Hi my friend, character length has to be a number between 8-128. Please select again.");
+      return false;
+    }
+
+   if (confirm("Do you want your password to include numbers?")) {
+      responsesArr = responsesArr.concat(numberArr);
+   }
+
+    if (confirm("Do you want your password to include lower case letters?")) {
+      responsesArr = responsesArr.concat(lowerCaseArr);
+    }
+
+    if (confirm("Do you want your password to include upper case letters?")) {
+      responsesArr = responsesArr.concat(upperCaseArr);
+    }
+
+    if (confirm("Do you want your password to include special characters?")) {
+      responsesArr = responsesArr.concat(specialCharacterArr);
+    }
+    return true;
+}
